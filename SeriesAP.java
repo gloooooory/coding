e33a7f717d0c8d68cf55d451b1b81f17047f6614
gloooooory <glo8ory@gmail.com>
2021-11-15 12:44:06 +0530

    Series AP

---
+++ SeriesAP.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class SeriesAP {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+
+        int t = kb.nextInt();
+        while (t-- > 0) {
+            int a1 = kb.nextInt();
+            int a2 = kb.nextInt();
+            int n = kb.nextInt();
+            Solution s = new Solution();
+            int result = s.nthTermOfAP(a1, a2, n);
+            System.out.println(result);
+        }
+    }
+}
+
+class Solution {
+    int nthTermOfAP(int A1, int A2, int N) {
+        // An = A1 + (N - 1) * (A2 - A1)
+        
+        int an = A1 + (N - 1) * (A2 - A1);
+        return an;
+    }
+}
