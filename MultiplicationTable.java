a2c74771a1a09aec451da1a87576258bab8bd6e9
gloooooory <glo8ory@gmail.com>
2021-11-15 12:40:11 +0530

    Multiplication table

---
+++ MultiplicationTable.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class GfG {
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int t = kb.nextInt();
+        while (t > 0) {
+            int n = kb.nextInt();
+            List<Integer> list = Solution.getTable(n);
+            printTable(list);
+            t--;
+        }
+    }
+
+    public static void printTable(List<Integer> list) {
+        for (int x : list) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+class Solution{
+    static ArrayList<Integer> getTable(int N){
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        for (int i = 1; i <= 10; i++) {
+            list.add(N * i);
+        }
+        
+        return list;
+    }
+}
