9a67b06378709977dd039f22b465d616676e49f6
gloooooory <glo8ory@gmail.com>
2021-12-26 18:38:35 +0530

    VIP: Nth Node from end

---
+++ 2021/Dec/25/NthNodeFromEnd.java
@@ -0,0 +1,128 @@
+/**
+ * Link: https://practice.geeksforgeeks.org/problems/nth-node-from-end-of-linked-list/1
+ * Date: 25th Dec 2021
+ * Code #3
+*/
+
+import java.util.*;
+
+import java.util.*;
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) 
+    {
+        data = d; 
+        next = null;
+    }
+}
+	
+
+	
+public class NthNodeFromEnd
+{
+    Node head;  
+	Node tail;
+	/* Function to print linked list */
+    void printList(Node head)
+    {
+        Node temp = head;
+        while (temp != null)
+        {
+           System.out.print(temp.data+" ");
+           temp = temp.next;
+        }  
+        System.out.println();
+    }
+	
+ 
+    /* Inserts a new Node at front of the list. */
+    public void addToTheLast(Node node) 
+	{
+
+		if (head == null) 
+		{
+			head = node;
+			tail = node;
+		} else 
+		{
+		    tail.next = node;
+		    tail = node;
+		}
+    }
+	  
+     /* Drier program to test above functions */
+    public static void main(String args[])
+    {
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			NthNodeFromEnd llist = new NthNodeFromEnd();
+			//int n=Integer.parseInt(br.readLine());
+			int a1=sc.nextInt();
+			Node head= new Node(a1);
+            llist.addToTheLast(head);
+            for (int i = 1; i < n; i++) 
+			{
+				int a = sc.nextInt(); 
+				llist.addToTheLast(new Node(a));
+			}
+          
+		//System.out.println(llist.head.data);
+        GfG g = new GfG(); 
+		//System.out.println(k);
+		System.out.println(g.getNthFromLast(llist.head,k));
+		
+			t--;
+		}
+	}
+}
+
+
+	
+	// } Driver Code Ends
+
+
+/* Structure of node
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {data = d; next = null; }
+}
+*/
+
+class GfG
+{
+    //Function to find the data of nth node from the end of a linked list.
+    int getNthFromLast(Node head, int n)
+    {
+    	int length = length(head);
+    	
+    	if (length < n) return -1;
+    	
+    	length = length - n;
+    	
+    	Node temp = head;
+    	while (length > 0) {
+    	    temp = temp.next;
+    	    length--;
+    	}
+    	return temp.data;
+    }
+    
+    int length(Node head) {
+        int count = 0;
+        while(head != null) {
+            head = head.next;
+            count++;
+        }
+        return count;
+    }
+}
