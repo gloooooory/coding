a0b955f239ba98a99037ab5140eb2f9155e30aaa
gloooooory <glo8ory@gmail.com>
2021-12-28 11:09:43 +0530

    VIP: Flattening a linked list

---
+++ 2021/Dec/28/FlatteningLinkedList.java
@@ -0,0 +1,151 @@
+/**
+ * Link: https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1#
+ * Date: 28 Dec 2021
+ * Code #2
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+
+
+class FlatteningLinkedList
+{	
+    Node head;
+	
+	void printList(Node node)
+    {
+        //Node temp = head;
+        while (node != null)
+        {
+            System.out.print(node.data + " ");
+            node =node.bottom;
+        }
+        System.out.println();
+    }
+	public  static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		FlatteningLinkedList list = new FlatteningLinkedList();
+		while(t>0)
+		{
+			int N = sc.nextInt();
+			int arr[] = new int[N];
+			for(int i=0;i<N;i++)
+				arr[i] = sc.nextInt();
+			
+			Node temp = null;
+			Node tempB = null;
+			Node pre = null;
+			Node preB = null;	
+			int flag=1;
+			int flag1=1;
+			for(int i=0; i<N;i++)
+			{
+				int m = arr[i];
+				m--;
+				int a1 = sc.nextInt();
+				temp = new Node(a1);
+				if(flag == 1)
+				{
+					list.head = temp;
+					pre = temp;
+					flag = 0;
+					flag1 = 1;
+				}
+				else
+				{
+					pre.next = temp;
+					pre = temp;
+					flag1 = 1;
+				}
+				
+				for(int j=0;j<m;j++)
+				{
+					int a = sc.nextInt();
+					tempB = new Node(a);
+					if(flag1 == 1)
+					{
+						temp.bottom = tempB;
+						preB = tempB;
+						flag1 = 0;
+					}
+					else
+					{
+						preB.bottom = tempB;
+						preB = tempB;
+					}
+				}
+				
+			}
+			//list.printList();
+			GfG g = new GfG();
+			Node root = g.flatten(list.head);
+			list.printList(root);
+		
+		t--;
+		}
+	}	
+}// } Driver Code Ends
+
+
+/*Node class  used in the program
+class Node
+{
+	int data;
+	Node next;
+	Node bottom;
+	
+	Node(int d)
+	{
+		data = d;
+		next = null;
+		bottom = null;
+	}
+}
+*/
+/*  Function which returns the  root of 
+    the flattened linked list. */
+class GfG {
+    
+    Node flatten(Node root) {
+	    if (root == null) return root;
+	    if (root.next == null) return root;
+	    Node head = root;
+	    root.next = flatten(root.next);
+	    root = merge(root, root.next);
+	    return root;
+    }
+    
+    Node merge(Node a, Node b) {
+        if (a == null) return b;
+        if (b == null) return a;
+        
+        Node res = null;
+        
+        if (a.data < b.data) {
+            res = a;
+            res.bottom = merge(a.bottom, b);
+        } else {
+            res = b;
+            res.bottom = merge(a, b.bottom);
+        }
+        
+        res.next = null;
+        return res;
+    }
+}
