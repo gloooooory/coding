e34cc99e997af6b4c2ab5e7a0f27f86336309e87
gloooooory <glo8ory@gmail.com>
2021-12-30 16:58:25 +0530

    VIP: Merge two sorted linked list

---
+++ 2021/Dec/30/MergeTwoSortedLinkedList.java
@@ -0,0 +1,121 @@
+/**
+ * Link: https://practice.geeksforgeeks.org/problems/merge-two-sorted-linked-lists/1#
+ * Date: 30 Dec 2021
+ * Code #4
+*/
+
+import java.util.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d) {
+        data = d; 
+        next = null;
+    }
+}
+
+
+class MergeTwoLinkedList
+{
+    Node head;
+
+  /* Function to print linked list */
+   public static void printList(Node head)
+    {
+        
+        while (head!= null)
+        {
+           System.out.print(head.data+" ");
+           head = head.next;
+        }  
+        System.out.println();
+    }
+	
+	 
+ 
+     /* Driver program to test above functions */
+    public static void main(String args[])
+    {
+        /* Constructed Linked List is 1->2->3->4->5->6->
+           7->8->8->9->null */
+         Scanner sc = new Scanner(System.in);
+		 int t=sc.nextInt();
+		 
+		 while(t>0)
+         {
+			int n1 = sc.nextInt();
+			int n2 = sc.nextInt();
+			Node head1 = new Node(sc.nextInt());
+            Node tail1 = head1;
+            for(int i=0; i<n1-1; i++)
+            {
+                tail1.next = new Node(sc.nextInt());
+                tail1 = tail1.next;
+            }
+			Node head2 = new Node(sc.nextInt());
+            Node tail2 = head2;
+            for(int i=0; i<n2-1; i++)
+            {
+                tail2.next = new Node(sc.nextInt());
+                tail2 = tail2.next;
+            }
+			
+			LinkedList obj = new LinkedList();
+			Node head = obj.sortedMerge(head1,head2);
+			printList(head);
+			
+			t--;
+			
+         }
+    }
+}
+// } Driver Code Ends
+
+
+/*
+  Merge two linked lists 
+  head pointer input could be NULL as well for empty list
+  Node is defined as 
+    class Node
+    {
+        int data;
+        Node next;
+        Node(int d) {data = d; next = null; }
+    }
+*/
+
+class LinkedList
+{
+    //Function to merge two sorted linked list.
+    Node sortedMerge(Node head1, Node head2) {
+        Node res = new Node(0);
+        Node curr = res;
+        
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                curr.next = head1;
+                head1 = head1.next;
+            } else {
+                curr.next = head2;
+                head2 = head2.next;
+            }
+            curr = curr.next;
+        }
+        
+        while (head1 != null) {
+            curr.next = head1;
+            head1 = head1.next;
+            curr = curr.next;
+        }
+        
+        while (head2 != null) {
+            curr.next = head2;
+            head2 = head2.next;
+            curr = curr.next;
+        }
+        
+        return res.next;
+   } 
+}
