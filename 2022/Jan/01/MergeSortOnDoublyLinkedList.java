1d801c70f7446f51e09667c03e58d6054c4e0464
gloooooory <glo8ory@gmail.com>
2022-01-02 00:00:08 +0530

    VIP: Merge sort on doubly linked list

---
+++ 2022/Jan/01/MergeSortOnDoublyLinkedList.java
@@ -0,0 +1,141 @@
+/**
+ * Link: https://practice.geeksforgeeks.org/problems/merge-sort-on-doubly-linked-list/1
+ * Date: 01 Jan 2022
+ * Code #5
+*/
+
+import java.util.*;
+import java.util.*;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int key)
+    {
+        data = key;
+        next = prev = null;
+    }
+}
+
+class MergeSortOnDoublyLinkedList
+{
+    public static void main (String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int t = sc.nextInt();
+	    
+	    while(t-- > 0)
+	    {
+	        int n = sc.nextInt();
+	        int a1 = sc.nextInt();
+	        Node head = new Node(a1);
+	        Node temp = head;
+	        
+	        for (int i = 1;i < n;i++)
+	        {
+	            int a = sc.nextInt();
+	            Node n1 = new Node(a);
+	            n1.prev = temp;
+	            temp.next = n1;
+	            temp = n1;
+	        }
+	        
+	        head = new LinkedList().sortDoubly(head);
+	        printList(head);
+	    }
+	}
+	public static void printList(Node node)
+	{
+	    Node temp = node;
+	    while(node != null)
+	    {
+	        System.out.print(node.data + " ");
+	        temp = node;
+	        node = node.next;
+	    }
+	    System.out.println();
+	    while(temp != null)
+	    {
+	        System.out.print(temp.data + " ");
+	        temp = temp.prev;
+	    }
+	    System.out.println();
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        next = prev = null;
+    }
+}
+*/
+class LinkedList
+{
+    //Function to sort the given doubly linked list using Merge Sort.
+    static Node sortDoubly(Node head)
+    {
+        return mergeSort(head);
+    }
+    
+    static Node split(Node head) {
+		Node fast = head, slow = head;
+		while (fast.next != null && fast.next.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		Node temp = slow.next;
+		slow.next = null;
+		return temp;
+	}
+
+	static Node mergeSort(Node node) {
+		if (node == null || node.next == null) {
+			return node;
+		}
+		Node second = split(node);
+
+		// Recur for left and right halves
+		node = mergeSort(node);
+		second = mergeSort(second);
+
+		// Merge the two sorted halves
+		return merge(node, second);
+	}
+
+	// Function to merge two linked lists
+	static Node merge(Node first, Node second) {
+		// If first linked list is empty
+		if (first == null) {
+			return second;
+		}
+
+		// If second linked list is empty
+		if (second == null) {
+			return first;
+		}
+
+		// Pick the smaller value
+		if (first.data < second.data) {
+			first.next = merge(first.next, second);
+			first.next.prev = first;
+			first.prev = null;
+			return first;
+		} else {
+			second.next = merge(first, second.next);
+			second.next.prev = second;
+			second.prev = null;
+			return second;
+		}
+	}
+}
